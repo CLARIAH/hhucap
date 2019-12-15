5cb3b813e73ff31560ac5f2524e016914085ad1c
rijpma <auke.rijpma@gmail.com>
2019-12-15 15:37:21 +0100

    update regex, dependencies, file paths

--- script/ads_process.R
+++ script/ads_process.R
@@ -1,13 +1,9 @@
 library(tm)
-library(stringr)
-library(plyr)
-library(dplyr)
+library(stringi))
 library(udpipe)
-library(tidyr)
-library(tidytext)
 
 # Import csv files (as dataframes)
-ads <- read.delim('~/Downloads/textmining/hhucap/scripts/metadata/83249531X_1900_output/1900.csv', header = T)
+ads <- read.delim('script/metadata/832495182_output/1915.csv', header = T)
 
 # Set functions
 #Tagger
@@ -24,10 +20,10 @@ unlist_f <- function(arg2){
 
 #Set Occupations Titles for Matching
 # Note! List of occupations is checked for duplicates in linux terminal -->
-oc_file <- read.table("~/Downloads/textmining/hhucap/scripts/output.txt")
+oc_file <- read.table("script/output.txt", header = TRUE)
 oc_file$V1 <- gsub("[^[:alpha:]]", "", oc_file$V1)
 oc_file$V1 <- gsub("\\b[[:alpha:]]{13,}\\b", '', oc_file$V1) #remove long occupations
-oc_file <- oc_file %>% filter(oc_file$V1 != '')
+oc_file <- subset(oc_file, oc_file$V1 != '')
 words <- paste0(oc_file$V1, collapse = "|")
 
 #Set POS-Tagger
@@ -35,7 +31,7 @@ dl <- udpipe_download_model(language = "dutch")
 udmodel_dutch <- udpipe_load_model(file = dl$file_model)
 
 ########################## TOTAL ####################################33333
-ads_f <- function(x) {
+ads_f <- function(x, words) {
   corpus<-Corpus(VectorSource(x$ocr))
   corpus<-tm_map(corpus, removeNumbers)
   corpus<-tm_map(corpus, content_transformer(tolower))
@@ -43,27 +39,25 @@ ads_f <- function(x) {
   corpus<-tm_map(corpus, removePunctuation)
   
   x$ocr<-unlist(as.list(corpus))  
+  x_subset <- x[stri_detect_regex(x$ocr, words), ]
   
-  subset <- x %>% 
-    filter(str_detect(x$ocr, words))
-  
-  subset$tags <- lapply(subset$ocr, tag_f)
-  subset$tags <- lapply(subset$tags, unlist_f)
- # subset$adjectives <- str_extract(subset$tags,"<?=\\_ADJ")
-  subset
+  x_subset$tags <- lapply(x_subset$ocr, tag_f)
+  x_subset$tags <- lapply(x_subset$tags, unlist_f)
+ # x_subset$adjectives <- str_extract(x_subset$tags,"<?=\\_ADJ")
+  x_subset
 }
 
-tags <- ads_f(ads)
+tags <- ads_f(ads, words = words)
 
 # Save output
 adstags_210_1910 <- tags
-adstags_210_1885$adj <- for(i in adstags_210_1885$tags){
+adstags_210_1910$adj <- for(i in adstags_210_1910$tags){
   string <- unlist(i)
   string <- as.character(string)
-  grepl(string, "ADJ")
+  stri_detect_regex(string, "ADJ")
 }
-write.table(tags, file = "~/Downloads/textmining/hhucap/scripts/metadata/852115210_1885_output/852115210-1885-tags.csv")
+# write.table(tags, file = "~/Downloads/textmining/hhucap/scripts/metadata/852115210_1885_output/852115210-1885-tags.csv")
 
-tags_$adjectives[1] <- str_extract(tags_$tags[1],"<?=\\_ADJ")
+tags$adjectives[1] <- stri_extract_all_regex(tags$tags[1],"(\\w+)\\_ADJ")
 
-tags_$adjectives[1] <- gsub("(?!ADJ\\b)", "", tags_$tags[1])
+tags$adjectives[1] <- gsub("(?!ADJ\\b)", "", tags$tags[1])
